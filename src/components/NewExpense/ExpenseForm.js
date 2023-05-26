d3fa3b51fbabf231168096410ed1237784b56fb1
Isha Khurana <ikhurana2709@gmail.com>
2023-05-26 16:32:39 +0530

    Clear input on submitting form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -55,6 +55,10 @@ const dateChangeHandler=(event)=>{
         date:new Date(enteredDate)
     };
     console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+
   };
   
   return (
@@ -62,17 +66,17 @@ const dateChangeHandler=(event)=>{
     <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler} />
+          <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
         </div>
 
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' steps='0.01' onChange={amountChangeHandler} />
+          <input type='number' min='0.01' steps='0.01' value={enteredAmount} onChange={amountChangeHandler} />
         </div>
 
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2018-01-01' steps='2023-12-31' onChange={dateChangeHandler} />
+          <input type='date' min='2018-01-01' steps='2023-12-31' value={enteredDate} onChange={dateChangeHandler} />
         </div>
     </div>
 
